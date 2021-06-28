f8048f7924bcb4038e5f6dfd816046e44d9d6f8d
priliTut <73940232+priliTut@users.noreply.github.com>
2021-06-28 09:24:24 +0300

    Create twenty-twenty-child.php

---
+++ twenty-twenty-child.php
@@ -0,0 +1,13 @@
+
+<?php 
+/*
+Theme Name:   twenty-twenty-child Theme
+Theme URI:    http://example.com/twenty-twenty-child/
+Description: Twenty Twenty Child Theme
+Author: Erez Priel
+Template: twentytwenty
+Version: 1.0.0
+*/
+
+
+?>
